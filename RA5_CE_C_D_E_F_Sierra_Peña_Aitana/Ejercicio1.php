d1dd3bd9eabab128b5a40ca23496cf0b70d05dd2
AitanaSP <saitana906@gmail.com>
2025-11-11 15:47:25 +0100

    commit1

---
+++ RA5_CE_C_D_E_F_Sierra_Peña_Aitana/Ejercicio1.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "10 9 8 7 6 5 4 3 2 1<br>";
+echo "9 8 7 6 5 4 3 2 1<br>";
+echo "8 7 6 5 4 3 2 1<br>";
+echo "7 6 5 4 3 2 1<br>";
+echo "6 5 4 3 2 1<br>";
+echo "5 4 3 2 1<br>";
+echo "4 3 2 1<br>";
+echo "3 2 1<br>";
+echo "2 1<br>";
+echo "1<br>";
+?>
