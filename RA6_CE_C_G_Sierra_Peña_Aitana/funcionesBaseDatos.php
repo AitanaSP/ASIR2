b1ba20c61d5afe93287a05d86a15e5850c204c0f
AitanaSP <saitana906@gmail.com>
2025-11-25 18:51:57 +0100

    commit 5

--- RA6_CE_C_G_Sierra_Peña_Aitana/funcionesBaseDatos.php
+++ RA6_CE_C_G_Sierra_Peña_Aitana/funcionesBaseDatos.php
@@ -4,29 +4,28 @@ include_once 'constantes.php';
 
 function getConexionPDO()
 {
-    global $usuario, $password;
-        $conexionPDO = new PDO('mysql:host=localhost;dbname=libros2', $usuario, $password);
+    try{
+        $conexionPDO = new PDO('mysql:host=localhost;dbname=libros2','root','');
         return $conexionPDO;
+
+    } catch (PDOException $e) {
+        echo "Error al conectar con la base de datos: " . $e->getMessage();
+        return false;
+    }
 }
-
-
 
 function getConexionPDO_sin_bbdd()
 {
-    $usuarioCorrectoNODB = new PDO('mysql:host=localhost', $usuario, $password);
+
 }
 
 function getConexionMySQLi()
 {
-    $usuarioCorrecto_MySQLi = new mysqli();
 
-    $usuarioCorrecto_MySQLi->connect('localhost', 'localhost', '', 'libros2');
 }
 function getConexionMySQLi_sin_bbdd()
 {
-    $usuarioCorrecto_MySQLi = new mysqli();
 
-    $usuarioCorrecto_MySQLi->connect('localhost', 'localhost', '');
 }
 
 
@@ -57,15 +56,20 @@ function usuarioCorrecto_MySQLi($usuario, $password)
 
 function usuarioCorrecto($usuario, $password)
 {
- $checkusuario = getConexionPDO();
-    $checkusuario = $checkusuario->query("SELECT * FROM logins WHERE usuario='$usuario'");
-    if ($checkusuario->rowCount() == 1)
-    {
-        $
-} 
+
+    $conexionPDO = getConexionPDO();
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $sql = "SELECT usuario, passwd FROM logins WHERE usuario = '$usuario' AND passwd = '$password'";
+
+     $resultado = $conexionPDO->query($sql);
 
 
-
+    if ($resultado && $resultado->rowCount() > 0) {
+        return true;
+}
+}
+}
 
 function registrarUsuario_MySQLi($usuario, $password)
 {
@@ -75,9 +79,15 @@ function registrarUsuario_MySQLi($usuario, $password)
 
 function registrarUsuario($usuario, $password)
 {
-  
-}
+    $conexionPDO = getConexionPDO();
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
 
+    $sqlreg = "INSERT INTO logins VALUES ('$usuario', '$password')";
+    if ($conexionPDO->query($sqlreg)) {
+        echo "Usuario registrado correctamente";
+}
+    }
+}
 function insertarLibro_MySQLi($titulo, $anyo, $precio, $fechaAdquisicion)
 {
  
@@ -86,11 +96,16 @@ function insertarLibro_MySQLi($titulo, $anyo, $precio, $fechaAdquisicion)
 
 function insertarLibro($titulo, $anyo, $precio, $fechaAdquisicion)
 {
-   
+    $conexionPDO = getConexionPDO();
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $sqlreglib = "INSERT INTO  libros (titulo, anyo_edicion, precio, fecha_adquisicion) VALUES ('$titulo', '$anyo', '$precio', '$fechaAdquisicion')";
+    if ($conexionPDO->query($sqlreglib)) {
+        echo "Libro registrado correctamente";
+        return true;
 }
-
-
-
+}
+}
 
 function getLibros_MySQLi()
 {
@@ -99,7 +114,16 @@ function getLibros_MySQLi()
 
 function getLibros()
 {
+    $conexionPDO = getConexionPDO();
+
     
+    $consulta = "SELECT numero_ejemplar, titulo, anyo_edicion, precio, fecha_adquisicion FROM libros";
+    $datos = $conexionPDO->query($consulta);
+
+    if($datos){
+        $libro = $datos->fetchAll(PDO::FETCH_OBJ);
+        return $libro;
+    }
 }
 
 function getLibrosTitulo_MySQLi()
@@ -118,7 +142,18 @@ function getLibrosTitulo()
 
 function borrarLibro($numeroEjemplar)
 {
-   
+    $conexionPDO = getConexionPDO();
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+}
+
+    $sql = "DELETE FROM libros WHERE numero_ejemplar = '$numeroEjemplar'";
+    if ($conexionPDO->query($sql)) {
+        echo "Libro borrado correctamente";
+       $mensaje = getLibrosPrecio($numeroEjemplar);
+        return $mensaje;
+
+}
 }
 
 function borrarLibro_MySQLi($numeroEjemplar)
@@ -177,7 +212,19 @@ function getLibrosAnyo_MySQLi($libro)
 
 function getLibrosPrecio($libro)
 {
+    $conexionPDO = getConexionPDO();
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
     
+    $numero_ejemplar = $libro;
+    
+    $sql = "SELECT precio FROM libros WHERE numero_ejemplar = '$libro'";
+
+     $resultado = $conexionPDO->query($sql);
+    if ($conexionPDO->query($sql)) {
+        $mensaje = $resultado->fetchColumn();
+        return $mensaje;
+}
+}
 }
 
 function getLibrosAnyo($libro)
